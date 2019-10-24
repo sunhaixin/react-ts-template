2b6dfe30a228816e91cdfc3a69845ad7c3418c5c
sunhaixin <505872716@qq.com>
2019-10-24 14:34:53 +0800

    feat: 添加sass-resources-loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/,
